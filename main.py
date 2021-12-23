589d30b7ffd88952088026a1606e344306be76fd
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2021-12-23 15:16:03 +0000

    Update main.py
    Bot now evaluates comment chain to not reply to a joke with preexisting punchline comment

--- main.py
+++ main.py
@@ -2,27 +2,40 @@ import praw
 import os
 import time
 
-# init reddit bot instance
+
 # environment secrets are made available to the bot instance when running on Heroku
 r = praw.Reddit(
     username = "EnPassantHolyHellBot",
-    password = os.environ['password'],
-    client_id = os.environ['client_id'],
-    client_secret = os.environ['client_secret'],
+    password = os.environ['PASSWORD'],
+    client_id = os.environ['CLIENT_ID'],
+    client_secret = os.environ['CLIENT_SECRET'],
     user_agent = "EnPassantHolyHellBot by github.com/cutwell"
-    )
+)
 
+# target R/AnarchyChess comments
 subr = r.subreddit('anarchychess')
+
+def check_comment(comment):
+    # try/except to handle rate limiting errors
+    try:
+        # detect keyphrase in body
+        if "google it" in comment.body.lower():
+            # traverse comment replies for punchline
+            for reply in comment.replies:
+                if "holy hell" in comment.replies.lower():
+                    return
+            
+            # else, reply punchline ourselves
+            source_tag = "[^(github)](https://github.com/cutwell/anarchychesscommentbot) ^|"
+            body = "Holy Hell!"
+            comment.reply(f"{body}\n{source_tag}")
+
+    except praw.exceptions.APIException:
+        # gracefully handle rate limiting
+        print("Probably hit rate limit, pausing for 60 seconds..")
+        time.sleep(60)
+        print("..Resuming")
 
 # stream new comments to sub posts
 for comment in subr.stream.comments(skip_existing=True):
-    try:
-        # detect joke setup (string "google it" in a comment body)
-        if "google it" in comment.body:
-            # comment punchline
-            comment.reply("holy hell")
-    except praw.exceptions.APIException:
-        # gracefully pause when hitting wait limit
-        print("Probably a rate limit, waiting 60 seconds..")
-        time.sleep(60)
-        print("..Resuming")
+    check_comment(comment)  # process comment and handle correctly
